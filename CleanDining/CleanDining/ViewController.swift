a07606682cd00731c789bb52a0a06c574ba21559
Eunice991217 <101406317+Eunice991217@users.noreply.github.com>
2023-06-20 01:55:10 +0900

    first commit

---
+++ CleanDining/CleanDining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanDining
+//
+//  Created by 김민경 on 2023/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
